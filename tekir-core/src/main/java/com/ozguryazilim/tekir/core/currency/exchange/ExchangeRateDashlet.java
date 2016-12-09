b6bc64ac4008ef75aa68f9a21a0e8cda4cee23f1
Ceyhun Onur <ceyhun.onur@ozguryazilim.com.tr>
2016-12-09 14:20:34 +0300

    refs #21902 dashlete kur değişimleri eklendi.

--- tekir-core/src/main/java/com/ozguryazilim/tekir/core/currency/exchange/ExchangeRateDashlet.java
+++ tekir-core/src/main/java/com/ozguryazilim/tekir/core/currency/exchange/ExchangeRateDashlet.java
@@ -9,10 +9,19 @@ import com.ozguryazilim.tekir.entities.ExchangeRate;
 import com.ozguryazilim.telve.dashboard.AbstractDashlet;
 import com.ozguryazilim.telve.dashboard.Dashlet;
 import com.ozguryazilim.telve.dashboard.DashletCapability;
+
+import java.math.BigDecimal;
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.Date;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
 import javax.inject.Inject;
+
+import org.apache.commons.collections.CollectionUtils;
 
 /**
  * Günlük kurları gösteren bir dashlet.
@@ -27,6 +36,8 @@ public class ExchangeRateDashlet extends AbstractDashlet{
     
     private Date date = new Date();
     private List<ExchangeRate> rates = new ArrayList<>();
+	private Map<String,BigDecimal> yesterdayRates = new HashMap<>();
+
     
     @Override
     public void load() {
@@ -45,6 +56,18 @@ public class ExchangeRateDashlet extends AbstractDashlet{
     
     private void populate() {
         rates = repository.findByDate(date);
+        Calendar calendar = Calendar.getInstance();
+		 calendar.setTime(date);
+	     calendar.add(Calendar.DATE, -1);
+		List<ExchangeRate> yls = repository.findByDate(calendar.getTime());
+	
+		if (!yls.isEmpty()) {
+			yesterdayRates = yls.stream().collect(HashMap<String, BigDecimal>::new, 
+	                (m, c) -> m.put(c.getBaseCurrency().getCurrencyCode() + "/" + c.getTermCurrency().getCurrencyCode(), c.getBuyRate()),
+	                (m, u) -> {});
+		}
+		
+		
     }
 
     public Date getDate() {
@@ -62,5 +85,29 @@ public class ExchangeRateDashlet extends AbstractDashlet{
     public void setRates(List<ExchangeRate> rates) {
         this.rates = rates;
     }
+    
+  //TRY bazlı kurlar
+  	public List<ExchangeRate> getTryRates() {
+
+  		List<ExchangeRate> tryRates = getRates().stream()
+  				.filter(r -> 	r.getTermCurrency().getCurrencyCode().equals("TRY")).collect(Collectors.toList());		
+  		return tryRates;
+  	}
+  	
+  	//Çapraz kurlar
+  	public List<ExchangeRate> getCrossRates() {
+  		
+  		return new ArrayList<ExchangeRate>(CollectionUtils.subtract(getRates(), getTryRates()));
+  	}
+    public int currencyStatus(ExchangeRate rate){
+		BigDecimal yesterdayRate = yesterdayRates.get(rate.getBaseCurrency().getCurrencyCode() + "/" +
+													  rate.getTermCurrency().getCurrencyCode());
+		if(yesterdayRate == null){
+		return 0;
+		}
+		
+		return rate.getBuyRate().compareTo(yesterdayRate);
+		
+	}
     
 }
