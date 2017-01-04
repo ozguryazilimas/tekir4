248d28111da14be699573c411403f0c4aef9f667
Ceyhun Onur <ceyhun.onur@ozguryazilim.com.tr>
2017-01-04 16:25:49 +0300

    refs #22331 finance account dashlet eklendi

---
+++ tekir-finance/src/main/java/com/ozguryazilim/finance/account/FinanceAccountTxnSumModel.java
@@ -0,0 +1,96 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.ozguryazilim.finance.account;
+
+import com.ozguryazilim.tekir.entities.Contact;
+import com.ozguryazilim.tekir.entities.FinanceAccount;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+import java.util.Currency;
+
+/**
+ * Finance Account Txn üserinde summary almak için model.
+ * 
+ * @author Ceyhun Onur
+ */
+public class FinanceAccountTxnSumModel implements Serializable{
+    
+	private String accountName;
+    private FinanceAccount  account;
+    private BigDecimal localBalance;
+    private Currency currency;
+    private BigDecimal balance;
+    private Integer rate;
+
+    public FinanceAccountTxnSumModel(FinanceAccount account, BigDecimal localBalance, BigDecimal balance, Currency currency) {
+        this.account = account;
+        this.localBalance = localBalance;
+        this.currency = currency;
+        this.balance = balance;
+        this.accountName = account.getName();
+    }
+     
+    public FinanceAccountTxnSumModel(FinanceAccount account, BigDecimal localBalance) {
+        this.account = account;
+        
+        //this.contactClass = contactClass;
+        this.localBalance = localBalance;
+        this.accountName = account.getName();
+        //this.currency = currency;
+    }
+
+    public FinanceAccount getAccount() {
+        return account;
+    }
+
+    public void setAccountName(FinanceAccount account) {
+        this.account = account;
+    }    
+    
+    public BigDecimal getLocalBalance() {
+        return localBalance;
+    }
+
+    public void setLocalBalance(BigDecimal localBalance) {
+        this.localBalance = localBalance;
+    }
+
+    public Currency getCurrency() {
+        return currency;
+    }
+
+    public void setCurrency(Currency currency) {
+        this.currency = currency;
+    }
+
+    public Integer getRate() {
+        return rate;
+    }
+
+    public void setRate(Integer rate) {
+        this.rate = rate;
+    }
+
+	public BigDecimal getBalance() {
+		return balance;
+	}
+
+	public void setBalance(BigDecimal balance) {
+		this.balance = balance;
+	}
+
+	public String getAccountName() {
+		return accountName;
+	}
+
+	public void setAccountName(String accountName) {
+		this.accountName = accountName;
+	}
+    
+    
+    
+}
