761b6e7bc1e8cd0748368e9fed9905d98fe2d450
Ceyhun Onur <ceyhun.onur@ozguryazilim.com.tr>
2017-01-12 15:57:23 +0300

    refs #22336 view ekran düzenlemeleri

--- tekir-commodity/src/main/java/com/ozguryazilim/tekir/commodity/CommodityHome.java
+++ tekir-commodity/src/main/java/com/ozguryazilim/tekir/commodity/CommodityHome.java
@@ -4,8 +4,16 @@ import com.ozguryazilim.tekir.core.currency.CurrencyService;
 import com.ozguryazilim.telve.forms.FormEdit;
 import com.ozguryazilim.telve.forms.FormBase;
 import com.ozguryazilim.tekir.entities.Commodity;
+import com.ozguryazilim.tekir.entities.TaxDefinition;
 import com.ozguryazilim.telve.data.RepositoryBase;
+import com.ozguryazilim.telve.entities.FeaturePointer;
 import com.ozguryazilim.telve.unit.UnitName;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Function;
+
 import javax.inject.Inject;
 
 /**
@@ -16,29 +24,71 @@ import javax.inject.Inject;
 @FormEdit( feature = CommodityFeature.class )
 public class CommodityHome extends FormBase<Commodity, Long> {
 
-        @Inject
-        private CurrencyService currencyService;
-    
+	@Inject
+	private CurrencyService currencyService;
+
 	@Inject
 	private CommodityRepository repository;
 
-        @Override
-        public void createNew() {
-            super.createNew(); 
-            getEntity().setDefaultCurrency(currencyService.getDefaultCurrency());
-        }
+	private List<TaxDefinition> taxList;
 
-        
-        
-        
+	@Override
+	public void createNew() {
+		super.createNew(); 
+		getEntity().setDefaultCurrency(currencyService.getDefaultCurrency());
+	}
+		
 	@Override
 	protected RepositoryBase<Commodity, CommodityViewModel> getRepository() {
 		return repository;
 	}
-        
-        public void onUnitSetChange(){
-            getEntity().setUnitName( new UnitName( getEntity().getUnitSet().getCode(), getEntity().getUnitSet().getBaseUnit()) );
-        }
-        
-        
+
+	public void onUnitSetChange(){
+		getEntity().setUnitName( new UnitName( getEntity().getUnitSet().getCode(), getEntity().getUnitSet().getBaseUnit()) );
+	}
+
+	public List<TaxDefinition> getTaxList() {		
+		buildTaxList();
+		return taxList;
+	}
+
+	public void setTaxList(List<TaxDefinition> taxList) {
+		this.taxList = taxList;
+	}
+
+	public FeaturePointer getFeaturePointer(){
+		FeaturePointer result = new FeaturePointer();
+		result.setBusinessKey(getEntity().getName());
+		result.setFeature(getFeatureClass().getSimpleName());
+		result.setPrimaryKey(getEntity().getId());
+		return result;
+	}
+
+	private void buildTaxList(){
+
+		taxList = new ArrayList<TaxDefinition>();
+
+		if(getEntity().getTax1() != null){
+			taxList.add(getEntity().getTax1());
+		}
+
+		if(getEntity().getTax2() != null){
+			taxList.add(getEntity().getTax2());
+		}
+
+		if(getEntity().getTax3() != null){
+			taxList.add(getEntity().getTax3());
+		}
+	}
+
+	public BigDecimal getTotalTaxRate(){
+		BigDecimal result = BigDecimal.ZERO;
+		if(getTaxList().size() != 0){
+			Function<TaxDefinition, BigDecimal> rateMapper = tax -> tax.getRate();
+	        result = taxList.stream().map(rateMapper).reduce(BigDecimal.ZERO, BigDecimal::add);				
+		}
+		return result;
+	}
+
+
 }
