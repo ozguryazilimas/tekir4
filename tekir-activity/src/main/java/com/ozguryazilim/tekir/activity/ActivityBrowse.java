63665672a5b7c368c0d0d7cf04e65d5e98fb6614
Ceyhun Onur <ceyhun.onur@ozguryazilim.com.tr>
2017-02-21 15:21:49 +0300

    refs #22338 browse ekranları kolon & filtre düzenlemesi.

--- tekir-activity/src/main/java/com/ozguryazilim/tekir/activity/ActivityBrowse.java
+++ tekir-activity/src/main/java/com/ozguryazilim/tekir/activity/ActivityBrowse.java
@@ -6,12 +6,26 @@
 package com.ozguryazilim.tekir.activity;
 
 import com.ozguryazilim.tekir.entities.Activity;
+import com.ozguryazilim.tekir.entities.ActivityPriority;
+import com.ozguryazilim.tekir.entities.ActivityStatus;
 import com.ozguryazilim.tekir.entities.Activity_;
+import com.ozguryazilim.tekir.entities.Contact_;
 import com.ozguryazilim.telve.data.RepositoryBase;
 import com.ozguryazilim.telve.forms.Browse;
 import com.ozguryazilim.telve.forms.BrowseBase;
 import com.ozguryazilim.telve.query.QueryDefinition;
+import com.ozguryazilim.telve.query.columns.DateColumn;
+import com.ozguryazilim.telve.query.columns.EnumColumn;
+import com.ozguryazilim.telve.query.columns.LinkColumn;
+import com.ozguryazilim.telve.query.columns.SubTextColumn;
 import com.ozguryazilim.telve.query.columns.TextColumn;
+import com.ozguryazilim.telve.query.filters.DateFilter;
+import com.ozguryazilim.telve.query.filters.DateValueType;
+import com.ozguryazilim.telve.query.filters.EnumFilter;
+import com.ozguryazilim.telve.query.filters.FilterOperand;
+import com.ozguryazilim.telve.query.filters.StringFilter;
+import com.ozguryazilim.telve.query.filters.SubStringFilter;
+
 import javax.inject.Inject;
 
 /**
@@ -26,17 +40,34 @@ public class ActivityBrowse extends BrowseBase<Activity, Activity> {
 
     @Override
     protected void buildQueryDefinition(QueryDefinition<Activity, Activity> queryDefinition) {
-        //queryDefinition
-        //        .addFilter(new StringFilter<>(Contact_.code, "general.label.Code"))
-        //        .addFilter(new StringFilter<>(Contact_.name, "general.label.Name"));
-
+    	EnumFilter statusFilter = new EnumFilter(Activity_.status, ActivityStatus.OPEN, "general.label.Status", "activity.status.");
+    	statusFilter.setOperand(FilterOperand.All);
+    	
+    	EnumFilter priorityFilter = new EnumFilter(Activity_.priority, ActivityPriority.HIGH, "general.label.Priority", "activity.priority.");
+		priorityFilter.setOperand(FilterOperand.All);
+    	
+        queryDefinition        		
+        		.addFilter(new StringFilter<>(Activity_.subject, "activity.label.Subject"))
+        		.addFilter(statusFilter)
+                .addFilter(priorityFilter)
+                .addFilter(new SubStringFilter<>(Activity_.person, Contact_.name, "general.label.Person"))
+                .addFilter(new SubStringFilter<>(Activity_.corporation, Contact_.name, "general.label.Corporation"))
+        		.addFilter(new DateFilter<>(Activity_.date, "general.label.Date", FilterOperand.All, DateValueType.LastMonth))
+        		.addFilter(new DateFilter<>(Activity_.dueDate, "activity.label.DueDate", FilterOperand.All, DateValueType.NextMonth));
+        
         queryDefinition
                 .addColumn(new TextColumn<>(Activity_.subject, "activity.label.Subject"), true)
-                .addColumn(new TextColumn<>(Activity_.body, "activity.label.Body"), true);
-                //.addColumn(new SubTextColumn<>(Contact_.primaryMobile, ContactPhone_.address, "contact.label.PrimaryMobile"), true)
-                //.addColumn(new SubTextColumn<>(Contact_.primaryPhone, ContactPhone_.address, "contact.label.PrimaryPhone"), true)
-                //.addColumn(new SubTextColumn<>(Contact_.primaryEmail, ContactEMail_.address, "contact.label.PrimaryEmail"), true)
-                //.addColumn(new TextColumn<>(Contact_.info, "general.label.Info"), true);
+                .addColumn(new TextColumn<>(Activity_.body, "activity.label.Body"), true)
+                .addColumn(new SubTextColumn<>(Activity_.person, Contact_.name, "general.label.Person"), true)
+                .addColumn(new SubTextColumn<>(Activity_.corporation, Contact_.name, "general.label.Corporation"), false)
+                .addColumn(new EnumColumn<>(Activity_.priority, "general.label.Priority", "activity.priority."),true)
+                .addColumn(new EnumColumn<>(Activity_.status, "general.label.Status", "activity.status."),true)
+                .addColumn(new DateColumn<>(Activity_.date, "general.label.Date"), false)
+        		.addColumn(new TextColumn<>(Activity_.assignee, "activity.label.Assignee"), false)
+                .addColumn(new DateColumn<>(Activity_.date, "general.label.Date"), false)
+                .addColumn(new DateColumn<>(Activity_.dueDate, "activity.label.DueDate"), false);
+
+
     }
 
     @Override
