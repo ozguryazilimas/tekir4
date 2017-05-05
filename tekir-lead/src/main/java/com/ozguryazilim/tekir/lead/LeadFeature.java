151ab62c037666f8daae29066c06f686788bd925
furkan <mfg263@gmail.com>
2017-05-05 15:01:37 +0300

    refs #23736 LeadPages.LeadBrowse, gerekli property

--- tekir-lead/src/main/java/com/ozguryazilim/tekir/lead/LeadFeature.java
+++ tekir-lead/src/main/java/com/ozguryazilim/tekir/lead/LeadFeature.java
@@ -1,7 +1,14 @@
 package com.ozguryazilim.tekir.lead;
 
+import com.ozguryazilim.tekir.entities.Lead;
+import com.ozguryazilim.tekir.lead.config.LeadPages;
 import com.ozguryazilim.telve.feature.AbstractFeatureHandler;
+import com.ozguryazilim.telve.feature.Feature;
+import com.ozguryazilim.telve.feature.Page;
+import com.ozguryazilim.telve.feature.PageType;
 
+@Feature(permission = "lead", forEntity = Lead.class)
+@Page(type = PageType.BROWSE, page = LeadPages.LeadBrowse.class)
 public class LeadFeature extends AbstractFeatureHandler {
-	
+
 }
--- tekir-lead/src/main/java/com/ozguryazilim/tekir/lead/config/LeadPages.java
+++ tekir-lead/src/main/java/com/ozguryazilim/tekir/lead/config/LeadPages.java
@@ -1,7 +1,25 @@
 package com.ozguryazilim.tekir.lead.config;
 
-import com.ozguryazilim.telve.feature.Pages;
+import javax.enterprise.context.ApplicationScoped;
 
+import org.apache.deltaspike.jsf.api.config.view.Folder;
+import org.apache.deltaspike.jsf.api.config.view.View;
+
+import com.ozguryazilim.tekir.core.config.SalesNavigationSection;
+import com.ozguryazilim.telve.auth.SecuredPage;
+import com.ozguryazilim.telve.nav.Navigation;
+import com.ozguryazilim.telve.view.PageTitle;
+import com.ozguryazilim.telve.view.Pages;
+
+@ApplicationScoped
+@Folder(name = "./lead")
 public interface LeadPages extends Pages {
+
+	@View
+	@SecuredPage("lead")
+	@PageTitle("module.caption.LeadBrowse")
+	@Navigation(label = "module.caption.LeadBrowse", icon = "fa fa-puzzle-piece", section = SalesNavigationSection.class)
+	class LeadBrowse implements LeadPages {
+	}
 
 }
