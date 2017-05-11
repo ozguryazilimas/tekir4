f3595de817d505a858a4b932b12c66d486e42586
furkan <mfg263@gmail.com>
2017-05-11 12:26:47 +0300

    refs #23776 leadsearchhandler, propertiler

--- tekir-lead/src/main/java/com/ozguryazilim/tekir/lead/LeadFeature.java
+++ tekir-lead/src/main/java/com/ozguryazilim/tekir/lead/LeadFeature.java
@@ -6,12 +6,14 @@ import com.ozguryazilim.telve.feature.AbstractFeatureHandler;
 import com.ozguryazilim.telve.feature.Feature;
 import com.ozguryazilim.telve.feature.Page;
 import com.ozguryazilim.telve.feature.PageType;
+import com.ozguryazilim.telve.feature.search.Search;
 
 @Feature(permission = "lead", forEntity = Lead.class)
 @Page(type = PageType.BROWSE, page = LeadPages.LeadBrowse.class)
 @Page(type = PageType.EDIT, page = LeadPages.Lead.class)
 @Page(type = PageType.VIEW, page = LeadPages.LeadView.class)
 @Page(type = PageType.MASTER_VIEW, page = LeadPages.LeadMasterView.class)
+@Search(handler = LeadSearchHandler.class)
 public class LeadFeature extends AbstractFeatureHandler {
 
 }
