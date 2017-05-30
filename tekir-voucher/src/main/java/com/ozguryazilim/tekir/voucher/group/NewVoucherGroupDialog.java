487fc54863a49dc45eb45bdb21dd5d3ef11e88c9
furkan <mfg263@gmail.com>
2017-05-30 12:33:51 +0300

    refs #24045 NewVoucherGroupDialog->DialogBase extend

--- tekir-voucher/src/main/java/com/ozguryazilim/tekir/voucher/group/NewVoucherGroupDialog.java
+++ tekir-voucher/src/main/java/com/ozguryazilim/tekir/voucher/group/NewVoucherGroupDialog.java
@@ -5,23 +5,17 @@
  */
 package com.ozguryazilim.tekir.voucher.group;
 
-import com.ozguryazilim.tekir.entities.Corporation;
 import com.ozguryazilim.tekir.entities.VoucherGroup;
 import com.ozguryazilim.tekir.entities.VoucherGroupStatus;
 import com.ozguryazilim.tekir.voucher.config.VoucherPages;
-import com.ozguryazilim.tekir.voucher.process.ProcessService;
-import com.ozguryazilim.telve.sequence.SequenceManager;
+import com.ozguryazilim.telve.view.DialogBase;
 
 import java.io.Serializable;
-import java.util.HashMap;
-import java.util.Map;
 import javax.enterprise.context.SessionScoped;
 import javax.inject.Inject;
 import javax.inject.Named;
-import org.apache.deltaspike.core.api.config.view.metadata.ViewConfigResolver;
-import org.apache.deltaspike.core.api.config.view.navigation.NavigationParameterContext;
-import org.apache.deltaspike.core.api.config.view.navigation.ViewNavigationHandler;
-import org.primefaces.context.RequestContext;
+
+import org.apache.deltaspike.core.api.config.view.ViewConfig;
 
 /**
  * Yeni Grup Tanım Popup Controller
@@ -30,10 +24,7 @@ import org.primefaces.context.RequestContext;
  */
 @Named
 @SessionScoped
-public class NewVoucherGroupDialog implements Serializable {
-
-	@Inject
-	private ViewConfigResolver viewConfigResolver;
+public class NewVoucherGroupDialog extends DialogBase implements Serializable {
 
 	@Inject
 	private VoucherGroupService voucherGroupService;
@@ -42,30 +33,16 @@ public class NewVoucherGroupDialog implements Serializable {
 	private VoucherGroupRepository repository;
 
 	private VoucherGroup entity;
-
-	public void openDialog() {
+	
+	@Override
+	public void beforeOpenDialog() {
 		entity = new VoucherGroup();
-
-		openDialogImpl();
-	}
-
-	protected void openDialogImpl() {
-		Map<String, Object> options = new HashMap<>();
-		options.put("modal", true);
-		// options.put("draggable", false);
-		options.put("resizable", false);
-		options.put("width", "780");
-		options.put("height", "430");
-		// options.put("contentWidth", 780);
-		options.put("contentHeight", 450);
-		options.put("position", "center top");
-
-		RequestContext.getCurrentInstance().openDialog(getDialogName(), options, null);
 	}
 
 	/**
 	 * Yeni person'ı save eder.
 	 */
+	@Override
 	public void closeDialog() {
 		entity.setStatus(VoucherGroupStatus.ACTIVE);
 		voucherGroupService.saveVoucherGroup(entity);
@@ -73,25 +50,13 @@ public class NewVoucherGroupDialog implements Serializable {
 		// navigationParameterContext.addPageParameter("eid",
 		// voucherGroup.getId());
 		// viewNavigationHandler.navigateTo(VoucherGroupPages.VoucherGroupView.class);
-
-		RequestContext.getCurrentInstance().closeDialog(null);
+		
+		closeDialogWindow();
 	}
 
-	/**
-	 * Bir şey yapmadan çık.
-	 */
-	public void cancelDialog() {
-		RequestContext.getCurrentInstance().closeDialog(null);
-	}
-
-	/**
-	 * startPopup dialog adını döndürür.
-	 *
-	 * @return
-	 */
-	public String getDialogName() {
-		String viewId = viewConfigResolver.getViewConfigDescriptor(VoucherPages.Group.NewVoucherGroupPopup.class).getViewId();
-		return viewId.substring(0, viewId.indexOf(".xhtml"));
+	@Override
+	public Class<? extends ViewConfig> getDialogViewConfig() {
+		return VoucherPages.Group.NewVoucherGroupPopup.class;
 	}
 
 	public VoucherGroup getEntity() {
