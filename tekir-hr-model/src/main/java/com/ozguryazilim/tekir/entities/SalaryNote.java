bebf848030778392259d7679e236ee8ed92bc28d
Hakan Uygun <hakan.uygun@ozguryazilim.com.tr>
2017-06-16 20:21:12 +0300

    refs #24238 Maaş fişleri için model sınıflar hazırlandı

---
+++ tekir-hr-model/src/main/java/com/ozguryazilim/tekir/entities/SalaryNote.java
@@ -0,0 +1,104 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.ozguryazilim.tekir.entities;
+
+import java.util.ArrayList;
+import java.util.Currency;
+import java.util.Date;
+import java.util.List;
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.ForeignKey;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import org.hibernate.annotations.LazyCollection;
+import org.hibernate.annotations.LazyCollectionOption;
+
+/**
+ * Maaş Ödeme Talimat Fişi
+ * @author Hakan Uygun
+ */
+@Entity
+@Table(name = "THR_SALARY_NOTE")
+public class SalaryNote extends VoucherBase{
+
+    @Id 
+    @GeneratedValue(strategy = GenerationType.AUTO, generator = "genericSeq")
+    @Column(name = "ID")
+    private Long id;
+    
+    /**
+     * Ödemenin hangi hesaptan yapılacağı
+     */
+    @ManyToOne
+    @JoinColumn(name = "ACCOUNT_ID", foreignKey = @ForeignKey(name = "FK_SALARY_ACC"))
+    private FinanceAccount financeAccount;
+    
+    
+    @Column(name = "PAYMENT_DATE")
+    @Temporal(TemporalType.DATE)
+    private Date paymentDate;
+    
+    
+    @Column(name = "CCY")
+    private Currency currency;
+    
+    
+    @OneToMany(mappedBy = "master", cascade = CascadeType.ALL, orphanRemoval = true)
+    @LazyCollection(LazyCollectionOption.FALSE)
+    private List<SalaryNoteItem> items = new ArrayList<>();
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public FinanceAccount getFinanceAccount() {
+        return financeAccount;
+    }
+
+    public void setFinanceAccount(FinanceAccount financeAccount) {
+        this.financeAccount = financeAccount;
+    }
+
+    public Date getPaymentDate() {
+        return paymentDate;
+    }
+
+    public void setPaymentDate(Date paymentDate) {
+        this.paymentDate = paymentDate;
+    }
+
+    public Currency getCurrency() {
+        return currency;
+    }
+
+    public void setCurrency(Currency currency) {
+        this.currency = currency;
+    }
+
+    public List<SalaryNoteItem> getItems() {
+        return items;
+    }
+
+    public void setItems(List<SalaryNoteItem> items) {
+        this.items = items;
+    }
+    
+    
+    
+}
