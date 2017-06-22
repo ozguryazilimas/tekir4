7e27d777b663f470f0e0366b6a8d4ef277620a39
oktayuyar <oktyuyar@gmail.com>
2017-06-22 18:46:27 +0300

    refs #24273 Ficance'a giden outputEntityLink'lerin outputFeatureLink'e dönüştürülmesi yapıldı

--- tekir-finance/src/main/java/com/ozguryazilim/finance/account/FinanceAccountFeature.java
+++ tekir-finance/src/main/java/com/ozguryazilim/finance/account/FinanceAccountFeature.java
@@ -7,6 +7,7 @@ package com.ozguryazilim.finance.account;
 
 import com.ozguryazilim.finance.config.FinancePages;
 import com.ozguryazilim.tekir.entities.Contact;
+import com.ozguryazilim.tekir.entities.FinanceAccount;
 import com.ozguryazilim.telve.feature.AbstractFeatureHandler;
 import com.ozguryazilim.telve.feature.Feature;
 import com.ozguryazilim.telve.feature.Page;
@@ -16,7 +17,7 @@ import com.ozguryazilim.telve.feature.PageType;
  *
  * @author oyas
  */
-@Feature(permission = "financeAccount", forEntity = Contact.class )
+@Feature(permission = "financeAccount", forEntity = FinanceAccount.class )
 @Page( type = PageType.BROWSE, page = FinancePages.FinanceAccountBrowse.class )
 @Page( type = PageType.VIEW, page = FinancePages.FinanceAccountView.class )
 @Page( type = PageType.MASTER_VIEW, page = FinancePages.FinanceAccountMasterView.class )
