2ed6640bcb00d4f54ea150d951fd1ee6715b3713
oktayuyar <oktyuyar@gmail.com>
2017-07-03 16:37:56 +0300

    refs #24351 feature sınıfı oluşturuldu

--- tekir-hr/src/main/java/com/ozguryazilim/tekir/hr/employee/leave/EmployeeLeaveFeature.java
+++ tekir-hr/src/main/java/com/ozguryazilim/tekir/hr/employee/leave/EmployeeLeaveFeature.java
@@ -5,12 +5,16 @@
  */
 package com.ozguryazilim.tekir.hr.employee.leave;
 
+import javax.enterprise.inject.Default;
+
 import com.ozguryazilim.tekir.entities.EmployeeLeave;
 import com.ozguryazilim.tekir.hr.config.EmployeePages;
+import com.ozguryazilim.tekir.voucher.Voucher;
 import com.ozguryazilim.telve.feature.AbstractFeatureHandler;
 import com.ozguryazilim.telve.feature.Feature;
 import com.ozguryazilim.telve.feature.Page;
 import com.ozguryazilim.telve.feature.PageType;
+import com.ozguryazilim.telve.feature.search.Search;
 
 /**
 *
@@ -18,7 +22,11 @@ import com.ozguryazilim.telve.feature.PageType;
 */
 @Feature(permission = "employeeLeave", forEntity = EmployeeLeave.class )
 @Page( type = PageType.BROWSE, page = EmployeePages.EmployeeLeaveBrowse.class )
+@Page( type = PageType.VIEW, page = EmployeePages.EmployeeLeaveView.class )
+@Page( type = PageType.MASTER_VIEW, page = EmployeePages.EmployeeLeaveMasterView.class )
 @Page( type = PageType.EDIT, page = EmployeePages.EmployeeLeave.class )
+@Search(	handler	=	EmployeeLeaveHandler.class)
+@Voucher @Default
 public class EmployeeLeaveFeature extends AbstractFeatureHandler{
     
 }
