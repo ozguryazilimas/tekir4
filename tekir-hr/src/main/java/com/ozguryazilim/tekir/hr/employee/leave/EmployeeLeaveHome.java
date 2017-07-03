cbdc39c40953092b11c2ecdbad617354e05dc844
oktayuyar <oktyuyar@gmail.com>
2017-07-03 16:34:51 +0300

    refs #24236 home sınıfı oluşturuldu

--- tekir-hr/src/main/java/com/ozguryazilim/tekir/hr/employee/leave/EmployeeLeaveHome.java
+++ tekir-hr/src/main/java/com/ozguryazilim/tekir/hr/employee/leave/EmployeeLeaveHome.java
@@ -5,16 +5,45 @@
  */
 package com.ozguryazilim.tekir.hr.employee.leave;
 
+import java.util.List;
+
 import javax.inject.Inject;
 
 import org.apache.deltaspike.core.api.config.view.ViewConfig;
 import org.apache.deltaspike.core.api.config.view.navigation.ViewNavigationHandler;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
+import com.ozguryazilim.tekir.contact.ContactRepository;
+import com.ozguryazilim.tekir.contact.ContactRoleRegistery;
+import com.ozguryazilim.tekir.contact.RelatedContactRepository;
+import com.ozguryazilim.tekir.contact.information.ContactInformationRepository;
+import com.ozguryazilim.tekir.core.currency.CurrencyService;
+import com.ozguryazilim.tekir.entities.Contact;
+import com.ozguryazilim.tekir.entities.ContactInformation;
+import com.ozguryazilim.tekir.entities.Corporation;
+import com.ozguryazilim.tekir.entities.Employee;
 import com.ozguryazilim.tekir.entities.EmployeeLeave;
+import com.ozguryazilim.tekir.entities.Person;
+import com.ozguryazilim.tekir.entities.ProcessType;
+import com.ozguryazilim.tekir.entities.RelatedContact;
+import com.ozguryazilim.tekir.entities.VoucherState;
+import com.ozguryazilim.tekir.entities.VoucherStateEffect;
+import com.ozguryazilim.tekir.entities.VoucherStateType;
 import com.ozguryazilim.tekir.hr.config.EmployeePages;
 import com.ozguryazilim.tekir.hr.employee.EmployeeRepository;
+import com.ozguryazilim.tekir.voucher.VoucherFormBase;
+import com.ozguryazilim.tekir.voucher.VoucherPrintOutAction;
+import com.ozguryazilim.tekir.voucher.VoucherStateAction;
+import com.ozguryazilim.tekir.voucher.VoucherStateConfig;
+import com.ozguryazilim.tekir.voucher.process.ProcessService;
 import com.ozguryazilim.telve.auth.Identity;
 import com.ozguryazilim.telve.data.RepositoryBase;
+import com.ozguryazilim.telve.entities.EntityBase;
+import com.ozguryazilim.telve.entities.FeaturePointer;
+import com.ozguryazilim.telve.feature.FeatureHandler;
+import com.ozguryazilim.telve.feature.FeatureRegistery;
+import com.ozguryazilim.telve.feature.FeatureUtils;
 import com.ozguryazilim.telve.forms.FormBase;
 import com.ozguryazilim.telve.forms.FormEdit;
 
@@ -23,21 +52,55 @@ import com.ozguryazilim.telve.forms.FormEdit;
 * @author oktay
 */
 @FormEdit(feature = EmployeeLeaveFeature.class)
-public class EmployeeLeaveHome extends FormBase<EmployeeLeave, Long>{
+public class EmployeeLeaveHome extends VoucherFormBase<EmployeeLeave>{
 
+	private static Logger LOG = LoggerFactory.getLogger(EmployeeLeaveHome.class);
 	
     @Inject
     private Identity identity;
+    
+    @Inject
+    private ViewNavigationHandler viewNavigationHandler;
 
     @Inject
     private EmployeeLeaveRepository repository;
 
 	@Override
-	protected RepositoryBase<EmployeeLeave, EmployeeLeaveViewModel> getRepository() {
-		// TODO Auto-generated method stub
+	protected RepositoryBase<EmployeeLeave, ?> getRepository() {
 		return repository;
 	}
-    public Class<? extends ViewConfig> newEmployeeLeave() {
-        return EmployeePages.EmployeeLeave.class;
+	
+    @Override
+    public boolean onBeforeSave() {
+        
+        if( getEntity().getState().equals(VoucherState.DRAFT) && !getEntity().isPersisted()){
+            getEntity().setState(VoucherState.OPEN);
+        }
+
+        if( getEntity().getId() == null ) {
+            getEntity().setId(getEntity().getId());
+            }
+        
+        return super.onBeforeSave(); //To change body of generated methods, choose Tools | Templates.
     }
+
+    @Override
+    protected VoucherStateConfig buildStateConfig() {
+        VoucherStateConfig config = new VoucherStateConfig();        
+        
+        config.addTranstion(VoucherState.DRAFT, new VoucherStateAction("publish", "fa fa-check" ), VoucherState.OPEN);
+        config.addTranstion(VoucherState.OPEN, new VoucherStateAction("approved", "fa fa-check" ), VoucherState.WON);
+        config.addTranstion(VoucherState.OPEN, new VoucherStateAction("cancel", "fa fa-ban", true ), VoucherState.CLOSE);
+        config.addTranstion(VoucherState.OPEN, new VoucherStateAction("revise", "fa fa-unlock", true ), VoucherState.DRAFT);
+        
+        config.addStateTypeAction(VoucherStateType.OPEN, new VoucherPrintOutAction(this));
+        config.addStateTypeAction(VoucherStateType.CLOSE, new VoucherPrintOutAction(this));
+        
+        return config;
+    }
+
+	public FeaturePointer getAllFeaturePointer(EntityBase contact){
+	   		return FeatureUtils.getFeaturePointer(contact);
+	}
+
 }
