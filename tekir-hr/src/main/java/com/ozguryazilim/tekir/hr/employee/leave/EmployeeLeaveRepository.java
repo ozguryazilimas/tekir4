26739cc591001ac96f484c3febb3f63355347124
oktayuyar <oktyuyar@gmail.com>
2017-07-03 16:32:13 +0300

    refs #24235 repository oluşturuldu

--- tekir-hr/src/main/java/com/ozguryazilim/tekir/hr/employee/leave/EmployeeLeaveRepository.java
+++ tekir-hr/src/main/java/com/ozguryazilim/tekir/hr/employee/leave/EmployeeLeaveRepository.java
@@ -1,3 +1,8 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
 package com.ozguryazilim.tekir.hr.employee.leave;
 
 import java.util.ArrayList;
@@ -18,20 +23,14 @@ import org.apache.deltaspike.data.api.criteria.CriteriaSupport;
 
 import com.google.common.base.Splitter;
 import com.google.common.base.Strings;
-import com.ozguryazilim.tekir.entities.AbstractPerson;
-import com.ozguryazilim.tekir.entities.Contact;
-import com.ozguryazilim.tekir.entities.ContactAddress;
-import com.ozguryazilim.tekir.entities.ContactEMail;
-import com.ozguryazilim.tekir.entities.ContactPhone;
-import com.ozguryazilim.tekir.entities.Corporation;
 import com.ozguryazilim.tekir.entities.Employee;
 import com.ozguryazilim.tekir.entities.EmployeeLeave;
 import com.ozguryazilim.tekir.entities.EmployeeLeave_;
 import com.ozguryazilim.tekir.entities.VoucherBase_;
+import com.ozguryazilim.tekir.entities.VoucherGroup;
+import com.ozguryazilim.tekir.entities.VoucherGroup_;
 import com.ozguryazilim.tekir.entities.Contact_;
-
-import com.ozguryazilim.tekir.voucher.Voucher;
-import com.ozguryazilim.telve.data.RepositoryBase;
+import com.ozguryazilim.tekir.voucher.VoucherRepositoryBase;
 import com.ozguryazilim.telve.query.QueryDefinition;
 import com.ozguryazilim.telve.query.filters.Filter;
 
@@ -41,7 +40,7 @@ import com.ozguryazilim.telve.query.filters.Filter;
  */
 @Repository
 @Dependent
-public abstract class EmployeeLeaveRepository extends RepositoryBase<EmployeeLeave, EmployeeLeaveViewModel>
+public abstract class EmployeeLeaveRepository extends VoucherRepositoryBase<EmployeeLeave, EmployeeLeaveViewModel>
 		implements CriteriaSupport<EmployeeLeave> {
 
 	private List<String> ownerFilter;
@@ -56,11 +55,13 @@ public abstract class EmployeeLeaveRepository extends RepositoryBase<EmployeeLea
 
 		// From Tabii ki EmployeeLeave
 		Root<EmployeeLeave> from = criteriaQuery.from(EmployeeLeave.class);
-		Join<EmployeeLeave, Employee> pm = from.join(EmployeeLeave_.employee, JoinType.LEFT);
+		Join<EmployeeLeave, Employee> joinEmployee = from.join(EmployeeLeave_.employee, JoinType.LEFT);
+		Join<EmployeeLeave, VoucherGroup> joinGroup = from.join(VoucherBase_.group, JoinType.LEFT);
 
 		// Sonuç filtremiz
 		buildVieModelSelect(criteriaQuery, from);
 
+		
 		// Filtreleri ekleyelim.
 		List<Predicate> predicates = new ArrayList<>();
 
@@ -71,10 +72,12 @@ public abstract class EmployeeLeaveRepository extends RepositoryBase<EmployeeLea
 		if (ownerFilter != null) {
 			predicates.add(from.get(VoucherBase_.owner).in(ownerFilter));
 		}
-
+		
+        
 		// Oluşan filtreleri sorgumuza ekliyoruz
 		criteriaQuery.where(predicates.toArray(new Predicate[] {}));
-
+		
+        
         // Öncelikle default sıralama verelim eğer kullanıcı tarafından tanımlı sıralama var ise onu setleyelim
         if (queryDefinition.getSorters().isEmpty()) {
             criteriaQuery.orderBy(criteriaBuilder.asc(from.get(EmployeeLeave_.employee).get(Contact_.name)));
@@ -87,6 +90,8 @@ public abstract class EmployeeLeaveRepository extends RepositoryBase<EmployeeLea
 		typedQuery.setMaxResults(queryDefinition.getResultLimit());
 		List<EmployeeLeaveViewModel> resultList = typedQuery.getResultList();
 
+		
+		System.out.println("veriler :"+resultList);
 		return resultList;
 
 	}
@@ -115,8 +120,9 @@ public abstract class EmployeeLeaveRepository extends RepositoryBase<EmployeeLea
 		CriteriaQuery<EmployeeLeaveViewModel> criteriaQuery = criteriaBuilder.createQuery(EmployeeLeaveViewModel.class);
 
 		Root<EmployeeLeave> from = criteriaQuery.from(EmployeeLeave.class);
-		Join<EmployeeLeave, Employee> pm = from.join(EmployeeLeave_.employee, JoinType.LEFT);
-
+		Join<EmployeeLeave, Employee> joinEmployee = from.join(EmployeeLeave_.employee, JoinType.LEFT);
+		Join<EmployeeLeave, VoucherGroup> joinGroup = from.join(VoucherBase_.group, JoinType.LEFT);
+		
 		// Sonuç filtremiz
 		buildVieModelSelect(criteriaQuery, from);
 
@@ -138,14 +144,16 @@ public abstract class EmployeeLeaveRepository extends RepositoryBase<EmployeeLea
 		// Oluşan filtreleri sorgumuza ekliyoruz
 		criteriaQuery.where(predicates.toArray(new Predicate[] {}));
 
-		 criteriaQuery.orderBy(criteriaBuilder.asc(from.get(EmployeeLeave_.employee).get(Contact_.name)));
+		criteriaQuery.orderBy(criteriaBuilder.asc(from.get(EmployeeLeave_.employee).get(Contact_.name)));
 		
 		// Haydi bakalım sonuçları alalım
 		TypedQuery<EmployeeLeaveViewModel> typedQuery = entityManager().createQuery(criteriaQuery);
 		typedQuery.setMaxResults(100);
 		List<EmployeeLeaveViewModel> resultList = typedQuery.getResultList();
 
-		return resultList;
+		
+		System.out.println("veriler :"+resultList);
+		return resultList;	
 	}
 
     @Override
@@ -153,17 +161,34 @@ public abstract class EmployeeLeaveRepository extends RepositoryBase<EmployeeLea
         throw new UnsupportedOperationException("Not supported yet.");
     }
 	
-	private void buildVieModelSelect(CriteriaQuery<EmployeeLeaveViewModel> criteriaQuery, Root<EmployeeLeave> from) {
-        criteriaQuery.multiselect(
+	private void buildVieModelSelect(CriteriaQuery<EmployeeLeaveViewModel> criteriaQuery, Root<? extends  EmployeeLeave> from) {
+        criteriaQuery.multiselect(    
                 from.get(EmployeeLeave_.id),
-                from.get(EmployeeLeave_.paid),
-                from.get(EmployeeLeave_.annual),
-                from.get(EmployeeLeave_.startDate),
-                from.get(EmployeeLeave_.endDate),
-                from.get(EmployeeLeave_.leaveDay),
+                from.get(VoucherBase_.code),
+                from.get(VoucherBase_.voucherNo),
+                from.get(VoucherBase_.info),
+                from.get(VoucherBase_.referenceNo),
+                
+                from.get(VoucherBase_.date),
+                from.get(VoucherBase_.owner),
+                from.get(VoucherBase_.state),
+                from.get(VoucherBase_.stateReason),
+                from.get(VoucherBase_.stateInfo),
+                
+        		from.get(VoucherBase_.group).get(VoucherGroup_.id),
+				from.get(VoucherBase_.group).get(VoucherGroup_.groupNo),
+				
+                from.get(VoucherBase_.topic),
                 
                 from.get(EmployeeLeave_.employee).get(Contact_.id),
-                from.get(EmployeeLeave_.employee).get(Contact_.name)
+                from.get(EmployeeLeave_.employee).get(Contact_.name),                
+                from.get(EmployeeLeave_.paid),
+                from.get(EmployeeLeave_.annual),
+                
+                from.get(EmployeeLeave_.startDate),
+                from.get(EmployeeLeave_.endDate),
+                from.get(EmployeeLeave_.leaveDay)
+                
         );
 
 	}
