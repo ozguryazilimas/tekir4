17846e744e920d3045a9c4d17702546994291323
oktayuyar <oktyuyar@gmail.com>
2017-07-03 16:35:46 +0300

    refs #24351 browse sınıfı oluşturuldu

--- tekir-hr/src/main/java/com/ozguryazilim/tekir/hr/employee/leave/EmployeeLeaveBrowse.java
+++ tekir-hr/src/main/java/com/ozguryazilim/tekir/hr/employee/leave/EmployeeLeaveBrowse.java
@@ -13,6 +13,7 @@ import javax.inject.Inject;
 
 import com.ozguryazilim.tekir.entities.Employee;
 import com.ozguryazilim.tekir.entities.EmployeeLeave;
+import com.ozguryazilim.tekir.entities.Employee_;
 import com.ozguryazilim.tekir.entities.EmployeeLeave_;
 import com.ozguryazilim.tekir.entities.VoucherBase_;
 import com.ozguryazilim.tekir.core.filters.IntegerFilter;
@@ -21,11 +22,19 @@ import com.ozguryazilim.tekir.entities.Contact_;
 import com.ozguryazilim.tekir.hr.employee.EmployeeFeature;
 import com.ozguryazilim.tekir.hr.employee.EmployeeRepository;
 import com.ozguryazilim.tekir.hr.employee.EmployeeViewModel;
+import com.ozguryazilim.tekir.voucher.VoucherBrowseBase;
+import com.ozguryazilim.tekir.voucher.VoucherFormBase;
+import com.ozguryazilim.tekir.voucher.VoucherRepositoryBase;
 import com.ozguryazilim.telve.auth.Identity;
 import com.ozguryazilim.telve.data.RepositoryBase;
+import com.ozguryazilim.telve.entities.EntityBase;
+import com.ozguryazilim.telve.entities.FeaturePointer;
+import com.ozguryazilim.telve.feature.FeatureUtils;
 import com.ozguryazilim.telve.forms.Browse;
 import com.ozguryazilim.telve.forms.BrowseBase;
 import com.ozguryazilim.telve.query.QueryDefinition;
+import com.ozguryazilim.telve.query.columns.BooleanColumn;
+import com.ozguryazilim.telve.query.columns.DateColumn;
 import com.ozguryazilim.telve.query.columns.LinkColumn;
 import com.ozguryazilim.telve.query.columns.SubTextColumn;
 import com.ozguryazilim.telve.query.columns.TextColumn;
@@ -43,13 +52,16 @@ import com.ozguryazilim.telve.query.filters.SubStringFilter;
  * @author oktay
  */
 @Browse(feature = EmployeeLeaveFeature.class)
-public class EmployeeLeaveBrowse extends BrowseBase<EmployeeLeave, EmployeeLeaveViewModel> {
+public class EmployeeLeaveBrowse extends VoucherBrowseBase<EmployeeLeave, EmployeeLeaveViewModel> {
 
 	@Inject
 	private EmployeeLeaveRepository repository;
 
 	@Inject
 	private Identity identity;
+	
+    @Inject 
+    private EmployeeLeaveHome home;
 
 	@Override
 	protected void buildQueryDefinition(QueryDefinition<EmployeeLeave, EmployeeLeaveViewModel> queryDefinition) {
@@ -65,12 +77,13 @@ public class EmployeeLeaveBrowse extends BrowseBase<EmployeeLeave, EmployeeLeave
         		.addFilter(new IntegerFilter<>(EmployeeLeave_.leaveDay,"hr.label.LeaveDay"));
         		
 		queryDefinition
-				.addColumn(new LinkColumn<>(EmployeeLeave_.employee, "hr.label.Employee"), true)
-				.addColumn(new LinkColumn<>(EmployeeLeave_.paid, "hr.label.Paid"), true)
-				.addColumn(new LinkColumn<>(EmployeeLeave_.annual, "hr.label.Annual"), true)
-				.addColumn(new LinkColumn<>(EmployeeLeave_.startDate, "hr.label.StartDate"), true)
-				.addColumn(new LinkColumn<>(EmployeeLeave_.endDate, "hr.label.EndDate"), true)
-				.addColumn(new LinkColumn<>(EmployeeLeave_.leaveDay, "hr.label.LeaveDay"), true);
+				.addColumn(new LinkColumn<>(EmployeeLeave_.voucherNo, "voucher.label.VoucherNo"), true)
+				.addColumn(new SubTextColumn<>(EmployeeLeave_.employee, Contact_.name, "general.label.Name"), true)
+				.addColumn(new BooleanColumn<>(EmployeeLeave_.paid, "hr.label.Paid", "general.boolean.active."),true)
+				.addColumn(new BooleanColumn<>(EmployeeLeave_.annual, "hr.label.Annual", "general.boolean.active."),true)
+				.addColumn(new DateColumn<>(EmployeeLeave_.startDate, "hr.label.StartDate"), true)
+				.addColumn(new DateColumn<>(EmployeeLeave_.endDate, "hr.label.EndDate"), true)
+				.addColumn(new TextColumn<>(EmployeeLeave_.leaveDay, "hr.label.LeaveDay"), true);
 
 	}
 
@@ -97,11 +110,26 @@ public class EmployeeLeaveBrowse extends BrowseBase<EmployeeLeave, EmployeeLeave
 		return repository;
 	}
 
-	public EmployeeLeave getEmployee() {
+	public EmployeeLeave getEmployeeLeave() {
 		if (getSelectedItem() != null) {
 			return repository.findBy(getSelectedItem().getId());
 		} else {
 			return null;
 		}
 	}
+
+	@Override
+	public VoucherRepositoryBase<EmployeeLeave, EmployeeLeaveViewModel> getVoucherRepository() {
+		return repository;
+	}
+    
+    @Override
+    public VoucherFormBase<EmployeeLeave> getHome() {
+    	return home;
+    }
+	
+	public FeaturePointer getAllFeaturePointer(EntityBase contact){
+	   		return FeatureUtils.getFeaturePointer(contact);
+	}
+
 }
