9ba0923d96a191eb6d81d3e30ddc35199febc533
oktayuyar <oktyuyar@gmail.com>
2017-07-04 16:36:31 +0300

    refs #24233 ufak düzenlemeler yapıldı

--- tekir-hr/src/main/java/com/ozguryazilim/tekir/hr/employee/leave/EmployeeLeaveHome.java
+++ tekir-hr/src/main/java/com/ozguryazilim/tekir/hr/employee/leave/EmployeeLeaveHome.java
@@ -5,50 +5,34 @@
  */
 package com.ozguryazilim.tekir.hr.employee.leave;
 
-import java.util.List;
-
 import javax.inject.Inject;
 
-import org.apache.deltaspike.core.api.config.view.ViewConfig;
 import org.apache.deltaspike.core.api.config.view.navigation.ViewNavigationHandler;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import com.ozguryazilim.tekir.contact.ContactRepository;
-import com.ozguryazilim.tekir.contact.ContactRoleRegistery;
-import com.ozguryazilim.tekir.contact.RelatedContactRepository;
-import com.ozguryazilim.tekir.contact.information.ContactInformationRepository;
-import com.ozguryazilim.tekir.core.currency.CurrencyService;
-import com.ozguryazilim.tekir.entities.Contact;
-import com.ozguryazilim.tekir.entities.ContactInformation;
 import com.ozguryazilim.tekir.entities.Corporation;
 import com.ozguryazilim.tekir.entities.Employee;
 import com.ozguryazilim.tekir.entities.EmployeeLeave;
 import com.ozguryazilim.tekir.entities.Person;
 import com.ozguryazilim.tekir.entities.ProcessType;
-import com.ozguryazilim.tekir.entities.RelatedContact;
 import com.ozguryazilim.tekir.entities.VoucherState;
-import com.ozguryazilim.tekir.entities.VoucherStateEffect;
 import com.ozguryazilim.tekir.entities.VoucherStateType;
-import com.ozguryazilim.tekir.hr.config.EmployeePages;
-import com.ozguryazilim.tekir.hr.employee.EmployeeRepository;
 import com.ozguryazilim.tekir.voucher.VoucherFormBase;
 import com.ozguryazilim.tekir.voucher.VoucherPrintOutAction;
 import com.ozguryazilim.tekir.voucher.VoucherStateAction;
 import com.ozguryazilim.tekir.voucher.VoucherStateConfig;
-import com.ozguryazilim.tekir.voucher.process.ProcessService;
 import com.ozguryazilim.telve.auth.Identity;
 import com.ozguryazilim.telve.data.RepositoryBase;
 import com.ozguryazilim.telve.entities.EntityBase;
 import com.ozguryazilim.telve.entities.FeaturePointer;
-import com.ozguryazilim.telve.feature.FeatureHandler;
-import com.ozguryazilim.telve.feature.FeatureRegistery;
 import com.ozguryazilim.telve.feature.FeatureUtils;
-import com.ozguryazilim.telve.forms.FormBase;
 import com.ozguryazilim.telve.forms.FormEdit;
+import com.ozguryazilim.telve.reports.JasperReportHandler;
 
 /**
 *
+* EmployeeLeave View Controller
 * @author oktay
 */
 @FormEdit(feature = EmployeeLeaveFeature.class)
@@ -64,6 +48,9 @@ public class EmployeeLeaveHome extends VoucherFormBase<EmployeeLeave>{
 
     @Inject
     private EmployeeLeaveRepository repository;
+    
+    @Inject
+    private JasperReportHandler reportHandler;
 
 	@Override
 	protected RepositoryBase<EmployeeLeave, ?> getRepository() {
@@ -76,10 +63,6 @@ public class EmployeeLeaveHome extends VoucherFormBase<EmployeeLeave>{
         if( getEntity().getState().equals(VoucherState.DRAFT) && !getEntity().isPersisted()){
             getEntity().setState(VoucherState.OPEN);
         }
-
-        if( getEntity().getId() == null ) {
-            getEntity().setId(getEntity().getId());
-            }
         
         return super.onBeforeSave(); //To change body of generated methods, choose Tools | Templates.
     }
@@ -98,7 +81,7 @@ public class EmployeeLeaveHome extends VoucherFormBase<EmployeeLeave>{
         
         return config;
     }
-
+    
 	public FeaturePointer getAllFeaturePointer(EntityBase contact){
 	   		return FeatureUtils.getFeaturePointer(contact);
 	}
