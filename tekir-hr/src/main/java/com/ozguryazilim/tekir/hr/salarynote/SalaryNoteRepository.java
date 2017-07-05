b61360936e5be5f81787b4924237d070b15efcd4
oktayuyar <oktyuyar@gmail.com>
2017-07-05 18:58:05 +0300

    refs #24242 viewmodel ve repository sınıfları oluşturuldu

---
+++ tekir-hr/src/main/java/com/ozguryazilim/tekir/hr/salarynote/SalaryNoteRepository.java
@@ -0,0 +1,188 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.ozguryazilim.tekir.hr.salarynote;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import javax.enterprise.context.Dependent;
+import javax.persistence.TypedQuery;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Join;
+import javax.persistence.criteria.JoinType;
+import javax.persistence.criteria.Predicate;
+import javax.persistence.criteria.Root;
+
+import org.apache.deltaspike.data.api.Repository;
+import org.apache.deltaspike.data.api.criteria.Criteria;
+
+import com.google.common.base.Strings;
+import com.ozguryazilim.tekir.entities.Contact;
+import com.ozguryazilim.tekir.entities.FinanceAccount;
+import com.ozguryazilim.tekir.entities.Process;
+import com.ozguryazilim.tekir.entities.SalaryNote;
+import com.ozguryazilim.tekir.entities.VoucherGroup;
+import com.ozguryazilim.tekir.entities.VoucherState;
+import com.ozguryazilim.tekir.entities.SalaryNote_;
+import com.ozguryazilim.tekir.entities.VoucherGroup_;
+import com.ozguryazilim.tekir.entities.VoucherBase_;
+import com.ozguryazilim.tekir.entities.Contact_;
+import com.ozguryazilim.tekir.entities.Process_;
+import com.ozguryazilim.tekir.entities.FinanceAccount_;
+import com.ozguryazilim.tekir.voucher.VoucherRepositoryBase;
+import com.ozguryazilim.telve.query.QueryDefinition;
+import com.ozguryazilim.telve.query.filters.Filter;
+
+
+/**
+ * Repository Class
+ * 
+ * @author oktay
+ *
+ */
+@Repository
+@Dependent
+public abstract class SalaryNoteRepository extends VoucherRepositoryBase<SalaryNote, SalaryNoteViewModel>{
+
+	 @Override
+	    public List<SalaryNoteViewModel>  browseQuery(QueryDefinition queryDefinition) {
+		 	
+		 List<Filter<SalaryNote, ?, ?>> filters = queryDefinition.getFilters();
+
+	        CriteriaBuilder criteriaBuilder = entityManager().getCriteriaBuilder();
+	        //Geriye PersonViewModel dönecek cq'yu ona göre oluşturuyoruz.
+	        CriteriaQuery<SalaryNoteViewModel> criteriaQuery = criteriaBuilder.createQuery(SalaryNoteViewModel.class);
+
+	        //From 
+	        Root<SalaryNote> from = criteriaQuery.from(SalaryNote.class);
+	        Join<SalaryNote, VoucherGroup> joinGroup = from.join(SalaryNote_.group, JoinType.LEFT);
+	        Join<SalaryNote, FinanceAccount> joinFinancAccount = from.join(SalaryNote_.financeAccount, JoinType.LEFT);
+	        
+	        //Sonuç filtremiz
+	        buildVieModelSelect(criteriaQuery, from);
+
+	        //Filtreleri ekleyelim.
+	        List<Predicate> predicates = new ArrayList<>();
+
+	        decorateFilters(filters, predicates, criteriaBuilder, from);
+
+	        buildSearchTextControl(queryDefinition.getSearchText(), criteriaBuilder, predicates, from);
+
+	        //RowLevel yetki kontrol filtresi
+	        buildOwnerFilter(predicates, from);
+
+	        //Oluşan filtreleri sorgumuza ekliyoruz
+	        criteriaQuery.where(predicates.toArray(new Predicate[]{}));
+
+	        // Öncelikle default sıralama verelim eğer kullanıcı tarafından tanımlı sıralama var ise onu setleyelim
+	        if (queryDefinition.getSorters().isEmpty()) {
+	            criteriaQuery.orderBy(criteriaBuilder.desc(from.get(VoucherBase_.date)));
+	        } else {
+	            criteriaQuery.orderBy(decorateSorts(queryDefinition.getSorters(), criteriaBuilder, from));
+	        }
+
+	        //Haydi bakalım sonuçları alalım
+	        TypedQuery<SalaryNoteViewModel> typedQuery = entityManager().createQuery(criteriaQuery);
+	        typedQuery.setMaxResults(queryDefinition.getResultLimit());
+	        List<SalaryNoteViewModel> resultList = typedQuery.getResultList();
+
+	        return resultList;
+		 
+	 }
+
+	    @Override
+	    public List<SalaryNote> suggestion(String searchText) {
+	        throw new UnsupportedOperationException("Not supported yet.");
+	    }
+
+	private void buildVieModelSelect(CriteriaQuery<SalaryNoteViewModel> criteriaQuery, Root<? extends SalaryNote> from) {
+		criteriaQuery.multiselect(
+                from.get(SalaryNote_.id),
+                from.get(VoucherBase_.code),
+                from.get(VoucherBase_.voucherNo),
+                from.get(VoucherBase_.info),
+                from.get(VoucherBase_.referenceNo),
+                from.get(VoucherBase_.date),
+                from.get(VoucherBase_.owner),
+                from.get(VoucherBase_.state),
+                from.get(VoucherBase_.stateReason),
+                from.get(VoucherBase_.stateInfo),
+                from.get(VoucherBase_.group).get(VoucherGroup_.id),
+                from.get(VoucherBase_.group).get(VoucherGroup_.groupNo),
+                from.get(VoucherBase_.topic),
+                from.get(SalaryNote_.financeAccount).get(FinanceAccount_.id),
+                from.get(SalaryNote_.financeAccount).get(FinanceAccount_.name),
+                from.get(SalaryNote_.financeAccount).get(FinanceAccount_.bank),
+                from.get(SalaryNote_.financeAccount).get(FinanceAccount_.iban),
+                from.get(SalaryNote_.paymentDate),
+                from.get(SalaryNote_.currency),
+                from.get(SalaryNote_.total)         
+        );
+		
+	}
+ 
+	private void buildSearchTextControl(String searchText, CriteriaBuilder criteriaBuilder, List<Predicate> predicates,
+			Root<SalaryNote> from) {
+		if (!Strings.isNullOrEmpty(searchText)) {
+            predicates.add(
+                    criteriaBuilder.or(
+                            criteriaBuilder.like(from.get(VoucherBase_.topic), "%" + searchText + "%"),
+                            criteriaBuilder.like(from.get(VoucherBase_.voucherNo), "%" + searchText + "%"))
+            );
+        }
+
+	}
+	
+    public List<SalaryNote> findByStateAndOwner(VoucherState state, String owner) {
+        Criteria<SalaryNote, SalaryNote> cr = criteria();
+
+        if (state != null) {
+            cr.eq(SalaryNote_.state, state);
+        }
+
+        if (!Strings.isNullOrEmpty(owner)) {
+            cr.eq(SalaryNote_.owner, owner);
+        }
+
+        return cr.createQuery().getResultList();
+
+    }
+    
+    public List<SalaryNoteViewModel> findExiperedSalesNotes(Date date) {
+        CriteriaBuilder criteriaBuilder = entityManager().getCriteriaBuilder();
+        //Geriye ViewModel dönecek cq'yu ona göre oluşturuyoruz.
+        CriteriaQuery<SalaryNoteViewModel> criteriaQuery = criteriaBuilder.createQuery(SalaryNoteViewModel.class);
+
+        //From 
+        Root<SalaryNote> from = criteriaQuery.from(SalaryNote.class);
+        Join<SalaryNote, VoucherGroup> joinGroup = from.join(VoucherBase_.group, JoinType.LEFT);
+        Join<SalaryNote, FinanceAccount> joinFinancAccount = from.join(SalaryNote_.financeAccount, JoinType.LEFT);
+
+        //Sonuç filtremiz
+        buildVieModelSelect(criteriaQuery, from);
+
+        //Filtreleri ekleyelim.
+        List<Predicate> predicates = new ArrayList<>();
+
+        //Tarih verilen parametreden küçük ve eşit olanlar.
+        predicates.add(criteriaBuilder.lessThanOrEqualTo(from.get(VoucherBase_.date), date));
+        
+        //Olumlu ya da Olumsuz kapanmamış olanlar.
+        predicates.add(criteriaBuilder.notLike(from.get(VoucherBase_.state).as(String.class), "CLOSE-%"));
+        
+        //Oluşan filtreleri sorgumuza ekliyoruz
+        criteriaQuery.where(predicates.toArray(new Predicate[]{}));
+
+        //Haydi bakalım sonuçları alalım
+        TypedQuery<SalaryNoteViewModel> typedQuery = entityManager().createQuery(criteriaQuery);
+        List<SalaryNoteViewModel> resultList = typedQuery.getResultList();
+
+        return resultList;
+    }
+    
+}
---
+++ tekir-hr/src/main/java/com/ozguryazilim/tekir/hr/salarynote/SalaryNoteViewModel.java
@@ -0,0 +1,85 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.ozguryazilim.tekir.hr.salarynote;
+
+import java.math.BigDecimal;
+import java.util.Currency;
+import java.util.Date;
+
+import com.ozguryazilim.tekir.entities.Contact;
+import com.ozguryazilim.tekir.entities.FinanceAccount;
+import com.ozguryazilim.tekir.entities.Process;
+import com.ozguryazilim.tekir.entities.VoucherGroup;
+import com.ozguryazilim.tekir.entities.VoucherState;
+import com.ozguryazilim.tekir.voucher.VoucherViewModel;
+
+/**
+ *SalaryNote View Model.
+ * 
+ * @author oktay
+ *
+ */
+public class SalaryNoteViewModel extends VoucherViewModel{
+	
+	private FinanceAccount financeAccount;
+	private Date paymentDate;
+	private Currency currency;
+	private BigDecimal total;
+	
+
+	public SalaryNoteViewModel(Long id, String code, String voucherNo, String info, String referenceNo, Date date,
+			String owner, VoucherState state, String stateReason, String stateInfo, VoucherGroup group, String topic,
+			FinanceAccount financeAccount, Date paymentDate, Currency currency, BigDecimal total) {
+		super(id, code, voucherNo, info, referenceNo, date, owner, state, stateReason, stateInfo, group, topic);
+		this.financeAccount = financeAccount;
+		this.paymentDate = paymentDate;
+		this.currency = currency;
+		this.total = total;
+	}
+
+	public SalaryNoteViewModel(Long id, String code, String voucherNo, String info, String referenceNo,Date date,
+			String owner, VoucherState state, String stateReason, String stateInfo, Long groupId,
+			String groupNo, String topic,Long financeAccountId,String financeAccountName, String financeAccountBank,
+			String financeAccountIban, Date paymentDate, BigDecimal total, Currency currency) {
+		super(id, code, voucherNo, info, referenceNo, date, owner, state, stateReason, stateInfo, groupId, groupNo, topic);
+		
+		
+		this.financeAccount = new FinanceAccount();
+		this.financeAccount.setId(financeAccountId);
+		this.financeAccount.setName(financeAccountName);
+		this.financeAccount.setBank(financeAccountBank);
+		this.financeAccount.setIban(financeAccountIban);
+		
+		this.paymentDate = paymentDate;
+		this.currency = currency;
+		this.total = total;
+	}
+	
+	public FinanceAccount getFinanceAccount() {
+		return financeAccount;
+	}
+	public void setFinanceAccount(FinanceAccount financeAccount) {
+		this.financeAccount = financeAccount;
+	}
+	public Date getPaymentDate() {
+		return paymentDate;
+	}
+	public void setPaymentDate(Date paymentDate) {
+		this.paymentDate = paymentDate;
+	}
+	public Currency getCurrency() {
+		return currency;
+	}
+	public void setCurrency(Currency currency) {
+		this.currency = currency;
+	}
+	public BigDecimal getTotal() {
+		return total;
+	}
+	public void setTotal(BigDecimal total) {
+		this.total = total;
+	}
+}
