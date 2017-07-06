9268fdc1510d2408caf9b0667b30a31a05770288
oktayuyar <oktyuyar@gmail.com>
2017-07-06 13:50:52 +0300

    refs #24242 home sınıfı oluşturuldu

---
+++ tekir-hr/src/main/java/com/ozguryazilim/tekir/hr/salarynote/SalaryNoteHome.java
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.ozguryazilim.tekir.hr.salarynote;
+
+import javax.inject.Inject;
+
+import org.apache.deltaspike.core.api.config.view.navigation.ViewNavigationHandler;
+import org.slf4j.LoggerFactory;
+
+import com.ozguryazilim.tekir.entities.SalaryNote;
+import com.ozguryazilim.tekir.voucher.VoucherFormBase;
+import com.ozguryazilim.tekir.voucher.VoucherStateConfig;
+import com.ozguryazilim.telve.auth.Identity;
+import com.ozguryazilim.telve.data.RepositoryBase;
+import com.ozguryazilim.telve.entities.EntityBase;
+import com.ozguryazilim.telve.entities.FeaturePointer;
+import com.ozguryazilim.telve.feature.FeatureUtils;
+import com.sun.istack.logging.Logger;
+
+/**
+ * @author oktay
+ *
+ */
+public class SalaryNoteHome extends VoucherFormBase<SalaryNote> {
+
+	@Inject
+	private Identity identity;
+
+	@Inject
+	private ViewNavigationHandler viewNavigationHandler;
+
+	@Inject
+	private SalaryNoteRepository repository;
+
+	@Override
+	protected RepositoryBase<SalaryNote, ?> getRepository() {
+		return repository;
+	}
+
+	@Override
+	protected VoucherStateConfig buildStateConfig() {
+		return null;
+	}
+
+}
