06c1d5d4cccf4b89ff2b089e6e39b67564467bca
Hüseyin Ateş <huseyin.ates@ozguryazilim.com.tr>
2018-04-05 17:58:41 +0300

    refs #23399 <permission target="..."> -> <permission name="...">

--- tekir-finance/src/main/java/com/ozguryazilim/finance/config/FinancePages.java
+++ tekir-finance/src/main/java/com/ozguryazilim/finance/config/FinancePages.java
@@ -1,13 +1,6 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.ozguryazilim.finance.config;
 
 import com.ozguryazilim.finance.virement.FinanceAccountVirementFeature;
-import com.ozguryazilim.tekir.contact.config.ContactPages;
-import com.ozguryazilim.tekir.core.config.ParamNavigationSection;
 import com.ozguryazilim.telve.auth.SecuredPage;
 import com.ozguryazilim.telve.nav.Navigation;
 import com.ozguryazilim.telve.view.PageTitle;
@@ -28,7 +21,9 @@ public interface FinancePages extends Pages {
     @View
     @SecuredPage("financeAccount")
     @PageTitle("module.caption.FinanceAccount")
-    @Navigation(label = "module.caption.FinanceAccount", icon = "flaticon-tax", section = FinanceNavigationSection.class)
+    @Navigation(label = "module.caption.FinanceAccount",
+            icon = "flaticon-tax",
+            section = FinanceNavigationSection.class)
     class FinanceAccountBrowse implements FinancePages {
     }
 
@@ -50,13 +45,13 @@ public interface FinancePages extends Pages {
     class FinanceAccountMasterView implements FinancePages {
     }
 
-    @SecuredPage
     @View
+    @SecuredPage
     class FinanceAccountLookup implements FinancePages {
     }
 
     @View
-    @SecuredPage("FinanceAccountVirement")
+    @SecuredPage("financeAccountVirement")
     @PageTitle("module.caption.FinanceAccountVirementBrowse")
     @Navigation(label = "module.caption.FinanceAccountVirementBrowse",
             icon = "flaticon-contract",
@@ -66,24 +61,24 @@ public interface FinancePages extends Pages {
     }
 
     @View
-    @SecuredPage("FinanceAccountVirement")
+    @SecuredPage("financeAccountVirement")
     @PageTitle("module.caption.FinanceAccountVirement")
     class FinanceAccountVirement implements FinancePages {
     }
 
     @View
-    @SecuredPage("FinanceAccountVirement")
+    @SecuredPage("financeAccountVirement")
     @PageTitle("module.caption.FinanceAccountVirement")
     class FinanceAccountVirementView implements FinancePages {
     }
 
     @View
-    @SecuredPage("FinanceAccountVirement")
+    @SecuredPage("financeAccountVirement")
     class FinanceAccountVirementMasterView implements FinancePages {
     }
 
-    @SecuredPage
     @View
+    @SecuredPage
     @PageTitle("report.name.financeAccountTxnReport")
     class FinanceAccountTxnReport implements FinancePages {
     }
