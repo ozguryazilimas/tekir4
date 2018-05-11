3facd201d247504a97b50ff295bca7df2cf5094d
Hüseyin Ateş <huseyin.ates@ozguryazilim.com.tr>
2018-05-11 17:39:10 +0300

    multiple inputTag support

--- tekir-core/src/main/java/com/ozguryazilim/tekir/core/view/InputTagController.java
+++ tekir-core/src/main/java/com/ozguryazilim/tekir/core/view/InputTagController.java
@@ -2,7 +2,7 @@ package com.ozguryazilim.tekir.core.view;
 
 import com.ozguryazilim.telve.entities.SuggestionItem;
 import com.ozguryazilim.telve.suggestion.SuggestionRepository;
-import org.omnifaces.cdi.ViewScoped;
+import org.apache.deltaspike.core.api.scope.WindowScoped;
 import org.primefaces.event.SelectEvent;
 
 import javax.el.ValueExpression;
@@ -11,18 +11,20 @@ import javax.faces.context.FacesContext;
 import javax.inject.Inject;
 import javax.inject.Named;
 import java.io.Serializable;
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
+import java.util.Map;
 import java.util.Set;
 import java.util.stream.Collectors;
 
 /**
  * t:inputTag bileşeni ardında çalışan controller
  *
- *
+ * @author Hüseyin ATEŞ
  */
 @Named
-@ViewScoped
+@WindowScoped
 public class InputTagController implements Serializable{
 
     public static final String SUGGESSTION_GROUP = "INPUT_TAG";
@@ -30,18 +32,19 @@ public class InputTagController implements Serializable{
     @Inject
     private SuggestionRepository repository;
 
-    private Set<String> potentialNewTags = new HashSet<>();
-    private Set<String> submittedNewTags = new HashSet<>();
+    private Map<String, Set<String>> potentialNewTagsMap = new HashMap<>();
+    private Map<String, Set<String>> submittedNewTagsMap = new HashMap<>();
 
     public List<TagResult> completeTag(String query) {
+        String key = getKey();
         List<String> suggestions = getSuggestionList();
         List<TagResult> resultList = suggestions.stream()
                 .filter(s -> s.contains(query))
                 .map(TagResult::new)
                 .collect(Collectors.toList());
 
-        if (!suggestions.contains(query) && !submittedNewTags.contains(query)) {
-            potentialNewTags.add(query);
+        if (!suggestions.contains(query) && !submittedNewTags(key).contains(query)) {
+            potentialNewTags(key).add(query);
             resultList.add(0, new TagResult(query, true));
         }
 
@@ -50,7 +53,8 @@ public class InputTagController implements Serializable{
 
     public void onItemSelect(SelectEvent event) {
         String tag = (String) event.getObject();
-        if (!submittedNewTags.contains(tag) && potentialNewTags.contains(tag)) {
+        String key = getKey();
+        if (!submittedNewTags(key).contains(tag) && potentialNewTags(key).contains(tag)) {
             SuggestionItem si = new SuggestionItem();
             si.setGroup(SUGGESSTION_GROUP);
             si.setKey(getKey());
@@ -58,7 +62,7 @@ public class InputTagController implements Serializable{
             si.setActive(Boolean.TRUE);
             si.setInfo("AUTO");
             repository.saveAndFlush(si);
-            submittedNewTags.add(tag);
+            submittedNewTags(key).add(tag);
         }
     }
 
@@ -73,6 +77,24 @@ public class InputTagController implements Serializable{
         UIComponent component = UIComponent.getCurrentComponent(fc);
         ValueExpression keyExp = (ValueExpression) component.getPassThroughAttributes().get("key");
         return (String) keyExp.getValue(fc.getELContext());
+    }
+
+    private Set<String> submittedNewTags(String key) {
+        if (!submittedNewTagsMap.containsKey(key)) {
+            Set<String> set = new HashSet<>();
+            submittedNewTagsMap.put(key, new HashSet<>());
+            return set;
+        }
+        return submittedNewTagsMap.get(key);
+    }
+
+    private Set<String> potentialNewTags(String key) {
+        if (!potentialNewTagsMap.containsKey(key)) {
+            Set<String> set = new HashSet<>();
+            potentialNewTagsMap.put(key, new HashSet<>());
+            return set;
+        }
+        return potentialNewTagsMap.get(key);
     }
 
     public class TagResult implements Serializable{
