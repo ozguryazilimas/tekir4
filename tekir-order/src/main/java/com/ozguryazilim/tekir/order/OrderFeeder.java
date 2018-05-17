3d44f02514734bbcc597b705a7d06fb85ac09ad9
soner.cirit <soner.cirit@ozguryazilim.com.tr>
2018-05-17 16:03:04 +0300

    refs #28235 orderFeeder duzenlendi

--- tekir-order/src/main/java/com/ozguryazilim/tekir/order/OrderFeeder.java
+++ tekir-order/src/main/java/com/ozguryazilim/tekir/order/OrderFeeder.java
@@ -20,6 +20,7 @@ import com.ozguryazilim.telve.auth.Identity;
 import com.ozguryazilim.telve.entities.FeaturePointer;
 import com.ozguryazilim.telve.forms.EntityChangeAction;
 import com.ozguryazilim.telve.forms.EntityChangeEvent;
+import com.ozguryazilim.telve.messages.Messages;
 import java.util.ArrayList;
 import java.util.List;
 import javax.inject.Inject;
@@ -117,17 +118,24 @@ public abstract class OrderFeeder<E extends Order> extends AbstractFeeder<E> {
 	 * @return
 	 */
 	protected String getMessage(VoucherStateChange event) {
+        String reason = event.getPayload().getStateReason();
+        if (reason == null) {
+            reason = Messages.getMessage("feed.messages.NullReason");
+        }
 		switch (event.getAction().getName()) {
 		case "CREATE":
 			return "feeder.messages.OrderFeeder.CREATE$%&" + identity.getUserName() + "$%&" + event.getPayload().getVoucherNo();
 		case "publish":
 			return "feeder.messages.OrderFeeder.PUBLISH$%&" + identity.getUserName() + "$%&" + event.getPayload().getVoucherNo();
 		case "revise":
-			return "feeder.messages.OrderFeeder.REVISE$%&" + identity.getUserName() + "$%&" + event.getPayload().getVoucherNo() + "$%&" + event.getPayload().getStateReason();
+            return "feeder.messages.OrderFeeder.REVISE$%&" + identity.getUserName() + "$%&" + event
+                .getPayload().getVoucherNo() + "$%&" + reason;
 		case "loss":
-			return "feeder.messages.OrderFeeder.LOST$%&" + event.getPayload().getVoucherNo() + "$%&" + event.getPayload().getStateReason();
+            return "feeder.messages.OrderFeeder.LOST$%&" + event.getPayload().getVoucherNo() + "$%&"
+                + reason;
 		case "cancel":
-			return "feeder.messages.OrderFeeder.CANCEL$%&" + identity.getUserName() + "$%&" + event.getPayload().getVoucherNo() + "$%&" + event.getPayload().getStateReason();
+            return "feeder.messages.OrderFeeder.CANCEL$%&" + identity.getUserName() + "$%&" + event
+                .getPayload().getVoucherNo() + "$%&" + reason;
         case "complete":
             return "feeder.messages.OrderFeeder.COMPLETE$%&" + identity.getUserName() + "$%&" + event.getPayload().getVoucherNo();
 		default:
