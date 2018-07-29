28c2a7b8ffa1f3be5e3eda9d511f2571da5f9350
Yusuf Unlu <myunlu35@gmail.com>
2018-07-29 16:22:57 +0300

    refs #29572 ApplicantBrowse ekranının gerçekleştirilmesi.

---
+++ tekir-recruit/src/main/java/com/ozguryazilim/tekir/recruit/applicant/ApplicantBrowse.java
@@ -0,0 +1,59 @@
+package com.ozguryazilim.tekir.recruit.applicant;
+
+import com.ozguryazilim.tekir.core.query.filter.TagFilter;
+import com.ozguryazilim.tekir.entities.Applicant;
+import com.ozguryazilim.tekir.entities.Applicant_;
+import com.ozguryazilim.telve.data.RepositoryBase;
+import com.ozguryazilim.telve.forms.Browse;
+import com.ozguryazilim.telve.forms.BrowseBase;
+import com.ozguryazilim.telve.query.QueryDefinition;
+import com.ozguryazilim.telve.query.columns.BooleanColumn;
+import com.ozguryazilim.telve.query.columns.DateColumn;
+import com.ozguryazilim.telve.query.columns.EnumColumn;
+import com.ozguryazilim.telve.query.columns.TextColumn;
+import com.ozguryazilim.telve.query.filters.BooleanFilter;
+import com.ozguryazilim.telve.query.filters.RatingFilter;
+import javax.inject.Inject;
+
+/**
+ *
+ * @author yusuf
+ */
+@Browse(feature=ApplicantFeature.class)
+public class ApplicantBrowse extends BrowseBase<Applicant, ApplicantViewModel>{
+
+    @Inject
+    private ApplicantRepository applicantRepository;
+      
+    @Override
+    protected void buildQueryDefinition(QueryDefinition<Applicant, ApplicantViewModel> queryDefinition) {
+        queryDefinition
+                .addFilter(new TagFilter<>("skills", "Applicant.label.Skills", "Applicant"))
+                .addFilter(new TagFilter<>("classifications", "Applicant.label.Classifications", "Applicant"))
+                .addFilter(new BooleanFilter<>(Applicant_.married, "Applicant.label.Married", "Married."))
+                .addFilter(new BooleanFilter<>(Applicant_.militaryDuty, "Applicant.label.militaryDuty", "militaryDuty."))             
+                .addFilter(new RatingFilter<>(Applicant_.rating,"Applicant.label.Rating",0,5));                       
+                
+        queryDefinition
+                .addColumn(new TextColumn<>(Applicant_.firstName, "Applicant.label.FirstName"),true)
+                .addColumn(new TextColumn<>(Applicant_.lastName, "Applicant.label.LastName"),true)
+                .addColumn(new DateColumn<>(Applicant_.birthDate, "Applicant.label.BirthDate"),true)
+                .addColumn(new BooleanColumn<>(Applicant_.married,"Applicant.label.Married","Married."),true)
+                .addColumn(new BooleanColumn<>(Applicant_.militaryDuty,"Applicant.label.militaryDuty","militaryDuty."),true)
+                .addColumn(new EnumColumn(Applicant_.gender,"Applicant.label.Gender","gender."),true)
+                ;
+    }
+
+    @Override
+    protected RepositoryBase<Applicant, ApplicantViewModel> getRepository() {
+        return applicantRepository;
+    }
+    
+     public Applicant getApplicant() {
+        if (getSelectedItem() != null) {
+            return applicantRepository.findBy(getSelectedItem().getId());
+        } else {
+            return null;
+        }
+    }     
+}
--- tekir-recruit/src/main/java/com/ozguryazilim/tekir/recruit/config/RecruitPages.java
+++ tekir-recruit/src/main/java/com/ozguryazilim/tekir/recruit/config/RecruitPages.java
@@ -10,6 +10,7 @@ import com.ozguryazilim.telve.nav.Navigation;
 import com.ozguryazilim.telve.view.PageTitle;
 import com.ozguryazilim.telve.view.Pages;
 import com.ozguryazilim.tekir.recruit.RecruitFeature;
+import com.ozguryazilim.tekir.recruit.applicant.ApplicantFeature;
 import javax.enterprise.context.ApplicationScoped;
 import org.apache.deltaspike.jsf.api.config.view.Folder;
 import org.apache.deltaspike.jsf.api.config.view.View;
@@ -22,31 +23,49 @@ import org.apache.deltaspike.jsf.api.config.view.View;
 @Folder(name = "./recruit")
 public interface RecruitPages extends Pages {
 
-    @View
-    @SecuredPage("recruit")
-    @PageTitle("module.caption.RecruitBrowse")
-    @Navigation(label = "module.caption.RecruitBrowse",
-            feature = RecruitFeature.class,
-            section = RecruitNavigationSection.class)
-    class RecruitBrowse implements RecruitPages {
-    }
+        @View
+        @SecuredPage("recruit")
+        @PageTitle("module.caption.RecruitBrowse")
+        @Navigation(label = "module.caption.RecruitBrowse",
+                feature = RecruitFeature.class,
+                section = RecruitNavigationSection.class)
+        class RecruitBrowse implements RecruitPages {
+        }
 
-    @View
-    @SecuredPage("recruit")
-    @PageTitle("module.caption.Recruit")
-    class Recruit implements RecruitPages {
-    }
+        @View
+        @SecuredPage("recruit")
+        @PageTitle("module.caption.Recruit")
+        class Recruit implements RecruitPages {
+        }
 
-    @View
-    @SecuredPage("recruit")
-    @PageTitle("module.caption.RecruitView")
-    class RecruitView implements RecruitPages {
-    }
+        @View
+        @SecuredPage("recruit")
+        @PageTitle("module.caption.RecruitView")
+        class RecruitView implements RecruitPages {
+        }
 
-    @View
-    @SecuredPage("recruit")
-    @PageTitle("module.caption.RecruitMasterView")
-    class RecruitMasterView implements RecruitPages {
+        @View
+        @SecuredPage("recruit")
+        @PageTitle("module.caption.RecruitMasterView")
+        class RecruitMasterView implements RecruitPages {
+        }
+
+    interface applicant extends RecruitPages{
+        
+        @View
+        @SecuredPage("applicant")
+        @PageTitle("module.caption.ApplicantBrowse")
+        @Navigation(label = "module.caption.ApplicantBrowse",
+                feature = ApplicantFeature.class,
+                section = RecruitNavigationSection.class)
+        class ApplicantBrowse implements RecruitPages{
+        }
+
+        @View
+        @SecuredPage
+        @PageTitle("module.caption.Applicant")
+        class Applicant implements RecruitPages{
+        }
     }
 
 }
