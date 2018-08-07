449c9a961960058ef7a4b115c8495a84921da10c
Hüseyin Ateş <huseyin.ates@ozguryazilim.com.tr>
2018-08-07 15:23:05 +0300

    refs #29572 refactoring

--- tekir-recruit/src/main/java/com/ozguryazilim/tekir/recruit/config/RecruitPages.java
+++ tekir-recruit/src/main/java/com/ozguryazilim/tekir/recruit/config/RecruitPages.java
@@ -1,13 +1,14 @@
 package com.ozguryazilim.tekir.recruit.config;
 
+import com.ozguryazilim.tekir.recruit.jobadvert.JobAdvertFeature;
 import com.ozguryazilim.telve.auth.SecuredPage;
 import com.ozguryazilim.telve.nav.Navigation;
 import com.ozguryazilim.telve.view.PageTitle;
 import com.ozguryazilim.telve.view.Pages;
-import com.ozguryazilim.tekir.recruit.jobadvert.JobAdvertFeature;
-import javax.enterprise.context.ApplicationScoped;
 import org.apache.deltaspike.jsf.api.config.view.Folder;
 import org.apache.deltaspike.jsf.api.config.view.View;
+
+import javax.enterprise.context.ApplicationScoped;
 
 /**
  *
@@ -17,31 +18,34 @@ import org.apache.deltaspike.jsf.api.config.view.View;
 @Folder(name = "./recruit")
 public interface RecruitPages extends Pages {
 
-    @View
-    @SecuredPage("recruit")
-    @PageTitle("module.caption.JobAdvertBrowse")
-    @Navigation(label = "module.caption.RecruitBrowse",
-            feature = JobAdvertFeature.class,
-            section = RecruitNavigationSection.class)
-    class JobAdvertBrowse implements RecruitPages {
-    }
+    @Folder(name = "./jobAdvert")
+    interface JobAdvertPages extends RecruitPages{
 
-    @View
-    @SecuredPage("recruit")
-    @PageTitle("module.caption.JobAdvert")
-    class JobAdvert implements RecruitPages {
-    }
+        @View
+        @SecuredPage("recruit")
+        @PageTitle("module.caption.JobAdvertBrowse")
+        @Navigation(label = "module.caption.RecruitBrowse",
+                feature = JobAdvertFeature.class,
+                section = RecruitNavigationSection.class)
+        class JobAdvertBrowse implements JobAdvertPages{
+        }
 
-    @View
-    @SecuredPage("recruit")
-    @PageTitle("module.caption.JobAdvert")
-    class JobAdvertView implements RecruitPages {
-    }
+        @View
+        @SecuredPage("recruit")
+        @PageTitle("module.caption.JobAdvert")
+        class JobAdvert implements JobAdvertPages{
+        }
 
-    @View
-    @SecuredPage("recruit")
-    @PageTitle("module.caption.JobAdvertMasterView")
-    class JobAdvertMasterView implements RecruitPages {
-    }
+        @View
+        @SecuredPage("recruit")
+        @PageTitle("module.caption.JobAdvert")
+        class JobAdvertView implements JobAdvertPages{
+        }
 
+        @View
+        @SecuredPage("recruit")
+        @PageTitle("module.caption.JobAdvertMasterView")
+        class JobAdvertMasterView implements JobAdvertPages{
+        }
+    }
 }
