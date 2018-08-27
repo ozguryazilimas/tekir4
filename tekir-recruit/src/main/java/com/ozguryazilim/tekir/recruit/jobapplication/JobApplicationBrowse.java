57c119957af7e3cc522f0e180641536f038869c8
Erdem Şahin <erdem.sahin.uslu@gmail.com>
2018-08-27 13:42:04 +0300

    refs #29569 properties refactor

--- tekir-recruit/src/main/java/com/ozguryazilim/tekir/recruit/jobapplication/JobApplicationBrowse.java
+++ tekir-recruit/src/main/java/com/ozguryazilim/tekir/recruit/jobapplication/JobApplicationBrowse.java
@@ -53,20 +53,20 @@ public class JobApplicationBrowse extends BrowseBase<JobApplication, JobApplicat
 
         queryDefinition
                 .addFilter(new SubStringFilter<>(JobApplication_.advert,JobAdvert_.topic,"JobAdvert.label.Topic"))
-                .addFilter(new DateFilter<>(JobApplication_.date,"JobApplication.label.Date",FilterOperand.All, DateValueType.LastMonth))
+                .addFilter(new DateFilter<>(JobApplication_.date,"general.label.Date",FilterOperand.All, DateValueType.LastMonth))
                 .addFilter(new SubDateFilter<>(JobApplication_.advert,JobAdvert_.startDate,"JobAdvert.label.startDate",FilterOperand.All, DateValueType.LastMonth))
                 .addFilter(new SubDateFilter<>(JobApplication_.advert,JobAdvert_.endDate,"JobAdvert.label.endDate",FilterOperand.All, DateValueType.LastMonth))
-                .addFilter(new SubStringFilter<>(JobApplication_.applicant,Applicant_.name,"JobApplication.label.Name"))
+                .addFilter(new SubStringFilter<>(JobApplication_.applicant,Applicant_.name,"general.label.Name"))
                 .addFilter(new UserFilter<>(JobApplication_.owner, "JobAdvert.label.Owner"))
                 .addFilter(new StringListFilter<>(JobApplication_.state,suggestionList,"JobApplication.label.State",""));
         
         queryDefinition
                 .addColumn(new SubLinkColumn<>(JobApplication_.advert,JobAdvert_.serial,"JobAdvert.label.Serial"),true)
-                .addColumn(new SubLinkColumn<>(JobApplication_.applicant,Applicant_.name,"JobApplication.label.Name"),true)
+                .addColumn(new SubLinkColumn<>(JobApplication_.applicant,Applicant_.name,"general.label.Name"),true)
                 .addColumn(new SubTextColumn<>(JobApplication_.advert,JobAdvert_.topic,"JobAdvert.label.Topic"),true)
                 .addColumn(new SubDateColumn<>(JobApplication_.advert,JobAdvert_.startDate,"JobAdvert.label.startDate"),true)
                 .addColumn(new SubDateColumn<>(JobApplication_.advert,JobAdvert_.endDate,"JobAdvert.label.endDate"),true)           
-                .addColumn(new DateColumn<>(JobApplication_.date,"JobApplication.label.Date"),true);
+                .addColumn(new DateColumn<>(JobApplication_.date,"general.label.Date"),true);
                 
     }
 
