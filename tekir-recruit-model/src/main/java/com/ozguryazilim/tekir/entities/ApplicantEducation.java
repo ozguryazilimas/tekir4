45a63e756a1232d775f72d6aab888a5e582e8b0e
Erdem Şahin <erdem.sahin.uslu@gmail.com>
2018-09-05 22:30:06 +0300

    refs #29973 applicant information modelleri icin hashCode/equals düzenlemesi ve gereksiz dosya silme

--- tekir-recruit-model/src/main/java/com/ozguryazilim/tekir/entities/ApplicantEducation.java
+++ tekir-recruit-model/src/main/java/com/ozguryazilim/tekir/entities/ApplicantEducation.java
@@ -1,12 +1,8 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.ozguryazilim.tekir.entities;
 
 import com.ozguryazilim.telve.entities.AuditBase;
 import java.util.Date;
+import java.util.Objects;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.ForeignKey;
@@ -122,7 +118,54 @@ public class ApplicantEducation extends AuditBase{
     public void setType(String type) {
         this.type = type;
     }
-    
-    
+
+    @Override
+    public int hashCode() {
+        int hash = 5;
+        hash = 59 * hash + Objects.hashCode(this.id);
+        hash = 59 * hash + Objects.hashCode(this.applicant);
+        hash = 59 * hash + Objects.hashCode(this.name);
+        hash = 59 * hash + Objects.hashCode(this.info);
+        hash = 59 * hash + Objects.hashCode(this.startDate);
+        hash = 59 * hash + Objects.hashCode(this.endDate);
+        hash = 59 * hash + Objects.hashCode(this.type);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final ApplicantEducation other = (ApplicantEducation) obj;
+        if (!Objects.equals(this.name, other.name)) {
+            return false;
+        }
+        if (!Objects.equals(this.info, other.info)) {
+            return false;
+        }
+        if (!Objects.equals(this.type, other.type)) {
+            return false;
+        }
+        if (!Objects.equals(this.id, other.id)) {
+            return false;
+        }
+        if (!Objects.equals(this.applicant, other.applicant)) {
+            return false;
+        }
+        if (!Objects.equals(this.startDate, other.startDate)) {
+            return false;
+        }
+        if (!Objects.equals(this.endDate, other.endDate)) {
+            return false;
+        }
+        return true;
+    }
     
 }
