943e58725711fb7a723fc7e77c4efc84e77049bd
Serdar Mutlu <serdarmutlu006@gmail.com>
2018-12-20 20:02:24 +0300

    refs #31062 jobAdvert seri numarası eşsiz olacak şekilde ayarlandı.

--- tekir-recruit-model/src/main/java/com/ozguryazilim/tekir/entities/JobAdvert.java
+++ tekir-recruit-model/src/main/java/com/ozguryazilim/tekir/entities/JobAdvert.java
@@ -16,6 +16,7 @@ import javax.persistence.Lob;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
+import javax.persistence.UniqueConstraint;
 import javax.validation.constraints.NotNull;
 
 /**
@@ -31,7 +32,7 @@ public class JobAdvert extends AuditBase{
     @Column(name = "ID")
     private Long id;
 
-    @Column(name = "CODE")
+    @Column(name = "CODE",unique = true)
     @BizKey @NotNull
     private String code;
     
