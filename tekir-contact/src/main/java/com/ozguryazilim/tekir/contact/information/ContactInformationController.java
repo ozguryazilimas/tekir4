3dcacb831efc0be393bdcc3db606d338b30d37e4
scirit <soner.cirit@ozguryazilim.com.tr>
2018-12-25 14:46:48 +0300

    refs #31110 changes validation method to regex

--- tekir-contact/src/main/java/com/ozguryazilim/tekir/contact/information/ContactInformationController.java
+++ tekir-contact/src/main/java/com/ozguryazilim/tekir/contact/information/ContactInformationController.java
@@ -2,9 +2,6 @@ package com.ozguryazilim.tekir.contact.information;
 
 import com.ozguryazilim.tekir.entities.ContactInformation;
 import java.io.Serializable;
-import java.net.MalformedURLException;
-import java.net.URISyntaxException;
-import java.net.URL;
 import javax.enterprise.context.SessionScoped;
 import javax.inject.Named;
 import org.apache.deltaspike.jpa.api.transaction.Transactional;
@@ -67,12 +64,17 @@ public class ContactInformationController implements Serializable {
         return info.getRoles().contains(ContactInformationConsts.Roles.PRIMARY);
     }
 
+    /**
+     * Kullanilan regex icin: https://gist.github.com/dperini/729294
+     * Version: 2018/09/12
+     * trim() metodu bastaki ve sondaki bosluk karakterlerini silmek icin kullaniliyor.
+     */
     public boolean isLink(String address) {
-        try {
-            new URL(address).toURI();
-            return true;
-        } catch (MalformedURLException | URISyntaxException e) {
-            return false;
-        }
+        String urlRegex = "^(?:(?:(?:https?|ftp):)?\\/\\/)(?:\\S+(?::\\S*)?@)?(?:(?!(?:10|127)(?:\\.\\d{1,3}){3})(?!(?"
+            + ":169\\.254|192\\.168)(?:\\.\\d{1,3}){2})(?!172\\.(?:1[6-9]|2\\d|3[0-1])(?:\\.\\d{1,3}){2})(?:[1-9]\\d?|1"
+            + "\\d\\d|2[01]\\d|22[0-3])(?:\\.(?:1?\\d{1,2}|2[0-4]\\d|25[0-5])){2}(?:\\.(?:[1-9]\\d?|1\\d\\d|2[0-4]\\d|2"
+            + "5[0-4]))|(?:(?:[a-z0-9\\u00a1-\\uffff][a-z0-9\\u00a1-\\uffff_-]{0,62})?[a-z0-9\\u00a1-\\uffff]\\.)+(?:[a"
+            + "-z\\u00a1-\\uffff]{2,}\\.?))(?::\\d{2,5})?(?:[/?#]\\S*)?$";
+        return address.trim().matches(urlRegex);
     }
 }
